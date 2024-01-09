60f7ff0d592d421cf2141d0d135aef576d51c3b9
yuriyknyazev <yurets.knyazev@mail.ru>
2024-01-09 22:55:03 +0200

    all tests are added

---
+++ tests/test_registration.py
@@ -0,0 +1,75 @@
+"""Проверка ошибки ввода некорректного пароля"""
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from constants import Constants
+from locators import Locators
+
+
+class TestRegistration:
+    def test_successful_registration(self, driver):
+        driver.get(Constants.URL)
+
+        # Клик по кнопке Личный кабинет
+        driver.find_element(*Locators.MAIN_PAGE_ENTER_PERSONAL_CABINET).click()
+
+        # Ожидание загрузки страницы
+        WebDriverWait(driver, 3).until(EC.url_to_be(Constants.LOGIN_PAGE_URL))
+
+        # Клик по кнопке Зарегистрироваться
+        driver.find_element(*Locators.PERSONAL_CABINET_REGISTER_BUTTON).click()
+
+        # Ожидание загрузки страницы
+        WebDriverWait(driver, 3).until(EC.url_to_be(Constants.REGISTRATION_PAGE_URL))
+
+        # Ввод имени
+        driver.find_element(*Locators.REGISTRATION_PAGE_NAME_ENTER).send_keys(*Constants.RANDOM_NAME)
+
+        # Ввод email
+        driver.find_element(*Locators.REGISTRATION_PAGE_EMAIL_ENTER).send_keys(Constants.RANDOM_EMAIL)
+
+        # Ввод пароля
+        driver.find_element(*Locators.REGISTRATION_PAGE_PASSWORD_ENTER).send_keys(Constants.PASSWORD)
+
+        # Клик по кнопке Зарегистрироваться
+        driver.find_element(*Locators.REGISTRATION_BUTTON_REGISTRATION_PAGE).click()
+
+        # Ожидание загрузки страницы
+        WebDriverWait(driver, 3).until(EC.url_to_be(Constants.LOGIN_PAGE_URL))
+
+        # Ввод email
+        driver.find_element(*Locators.LOGIN_PAGE_EMAIL_ENTER).send_keys(Constants.PERSONAL_EMAIL)
+
+        # Ввод пароля
+        driver.find_element(*Locators.LOGIN_PAGE_PASSWORD_ENTER).send_keys(Constants.PASSWORD)
+
+        # Клик по кнопке войти
+        driver.find_element(*Locators.LOGIN_PAGE_ENTER_BUTTON).click()
+
+        # Ожидание загрузки страницы
+        WebDriverWait(driver, 3).until(EC.url_to_be(Constants.URL))
+
+        # Проверка наличия кнопки Оформить заказ
+        assert driver.find_element(*Locators.MAKE_ORDER_BUTTON).text == "Оформить заказ"
+
+    def test_wrong_password(self, driver):
+        driver.get(Constants.URL)
+
+        # Клик по кнопке Личный кабинет
+        driver.find_element(*Locators.MAIN_PAGE_ENTER_PERSONAL_CABINET).click()
+
+        # Ожидание загрузки страницы
+        WebDriverWait(driver, 3).until(EC.url_to_be(Constants.LOGIN_PAGE_URL))
+
+        # Ввод email
+        driver.find_element(*Locators.LOGIN_PAGE_EMAIL_ENTER).send_keys(Constants.PERSONAL_EMAIL)
+
+        # Ввод некорректного пароля
+        driver.find_element(*Locators.LOGIN_PAGE_PASSWORD_ENTER).send_keys(12345)
+
+        # Клик по кнопке войти
+        driver.find_element(*Locators.LOGIN_PAGE_ENTER_BUTTON).click()
+
+        assert driver.find_element(*Locators.WRONG_PASSWORD_INSCRIPTION).text == "Некорректный пароль"
+
+
+
